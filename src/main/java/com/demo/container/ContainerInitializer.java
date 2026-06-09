d8927b0a11d4b808ca7159b053671cbdb70e3db0
Beverly Hills Money Gangster <beverly.hills.money.gangster@gmail.com>
2026-06-09 18:21:42 +0300

    Add more validation

--- src/main/java/com/demo/container/ContainerInitializer.java
+++ src/main/java/com/demo/container/ContainerInitializer.java
@@ -3,7 +3,9 @@ package com.demo.container;
 import com.demo.decorator.Decorator;
 import com.demo.factory.InstanceFactory;
 import com.demo.registry.ProfileRegistry;
+import java.lang.reflect.Modifier;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ContainerInitializer {
@@ -21,7 +23,22 @@ public class ContainerInitializer {
   }
 
   public void addDecorator(final Class<?> clazz, Decorator decorator) {
-    decorators.put(clazz, decorator);
+    if (clazz == null) {
+      throw new IllegalArgumentException("Can't decorate null");
+    } else if (decorator == null) {
+      throw new IllegalArgumentException("null can't be a decorator");
+    }
+    if (clazz.isInterface() || Modifier.isAbstract(clazz.getModifiers())) {
+      throw new IllegalStateException(
+          "Can't decorate class %s. Only concrete implementation can be decorated.".formatted(
+              clazz.getCanonicalName()));
+    }
+    Optional.ofNullable(decorators.putIfAbsent(clazz, decorator)).ifPresent(existingDecorator -> {
+      throw new IllegalStateException(
+          "Can't decorate class %s. Only one decorator per class is allowed. Check decorator %s"
+              .formatted(clazz.getCanonicalName(),
+                  existingDecorator.getClass().getCanonicalName()));
+    });
   }
 
   public Container init(final Class<?> mainClass) {
